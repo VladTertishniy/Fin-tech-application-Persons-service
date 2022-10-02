81761675e7130d097952577a89c890fd76486ff6
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-03 02:58:13 +0300

    added repository

---
+++ src/main/java/com/extrawest/persons_service/repository/OperatorRepository.java
@@ -0,0 +1,9 @@
+package com.extrawest.persons_service.repository;
+
+import com.extrawest.persons_service.model.Operator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperatorRepository extends JpaRepository<Operator, Long> {
+}
