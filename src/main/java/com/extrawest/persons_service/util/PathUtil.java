336585e27fa6350a2ab1844031ad72683cd0f7e1
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-03 02:58:25 +0300

    added utility class

---
+++ src/main/java/com/extrawest/persons_service/util/PathUtil.java
@@ -0,0 +1,13 @@
+package com.extrawest.persons_service.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathUtil {
+    public static final String OPERATORS_ROOT_PATH = "/operators";
+    public static final String CREATE_PATH = "/create";
+    public static final String UPDATE_PATH = "/update/{id}";
+    public static final String DELETE_PATH = "/delete/{id}";
+    public static final String GET_ALL_PATH = "/getAll";
+    public static final String GET_BY_ID_PATH = "/getById/{id}";
+}
